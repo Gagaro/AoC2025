2f0f195457a2eb577d299fdccb40604d32c3713f
Gagaro <gagaro42@gmail.com>
2025-12-09 09:19:49 +0100

    day 9

--- day9/run.py
+++ day9/run.py
@@ -1,16 +1,43 @@
-from collections import defaultdict
+from dataclasses import dataclass
 from pathlib import Path
 import sys
+
+from shapely import Polygon
 
 from runner import Runner
 
 
+@dataclass(eq=True, frozen=True)
+class Tile:
+    x: int
+    y: int
+
+    def area(self, other_tile: 'Tile') -> int:
+        return (abs(self.x - other_tile.x) + 1) * (abs(self.y - other_tile.y) + 1)
+
+
 class DayRunner(Runner):
     def run1(self):
-        return
+        tiles = [Tile(*map(int, line.split(','))) for line in self.lines]
+        max_area = 0
+        for index, tile in enumerate(tiles):
+            for second_tile in tiles[index + 1:]:
+                max_area = max(max_area, tile.area(second_tile))
+        return max_area
 
     def run2(self):
-        return
+        tiles = [Tile(*map(int, line.split(','))) for line in self.lines]
+        polygon = Polygon([(tile.x, tile.y) for tile in tiles])
+        max_area = 0
+        for index, tile in enumerate(tiles):
+            for second_tile in tiles[index + 1:]:
+                square = Polygon(
+                    ((tile.x, tile.y), (tile.x, second_tile.y),
+                     (second_tile.x, second_tile.y), (second_tile.x, tile.y))
+                )
+                if polygon.covers(square):
+                    max_area = max(max_area, tile.area(second_tile))
+        return max_area
 
 
 if __name__ == '__main__':
