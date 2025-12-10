5efb3c49faa98989a962f92aa1cec227bdcb13be
Gagaro <gagaro42@gmail.com>
2025-12-11 00:20:27 +0100

    day 10 part 1

--- day10/run.py
+++ day10/run.py
@@ -1,15 +1,46 @@
-from collections import defaultdict
+from collections import deque
 from pathlib import Path
 import sys
+from z3 import *
 
 from runner import Runner
 
 
 class DayRunner(Runner):
     def run1(self):
-        return
+        counts = []
+        for line in self.lines:
+            # print(line)
+
+            line = line.split(' ')
+            lights_goal = {i for i, c in enumerate(line[0][1:-1]) if c == '#'}
+            buttons = list(map(lambda x: set(map(int, x[1:-1].split(','))), line[1:-1]))
+
+            queue = deque([[set()]])
+            while queue:
+                states = queue.popleft()
+                state = states[-1]
+                for button in buttons:
+                    new_state = state ^ button
+                    if new_state == lights_goal:
+                        counts.append(len(states))
+                        break
+                    queue.append(states + [new_state])
+                else:
+                    continue
+                break
+        return sum(counts)
 
     def run2(self):
+        for line in self.lines:
+            print(line)
+
+            line = line.split(' ')
+            buttons = list(map(lambda x: set(map(int, x[1:-1].split(','))), line[1:-1]))
+            joltages = line[-1][1:-1].split(',')
+
+
+
         return
 
 
@@ -20,5 +51,5 @@ if __name__ == '__main__':
     else:
         input_path = Path(__file__).parent / 'input.txt'
 
-    print(DayRunner(input_path, test=is_test).run1())
+    # print(DayRunner(input_path, test=is_test).run1())
     print(DayRunner(input_path, test=is_test).run2())
