69fd0db57857725f8f26765489bd78f8be963b45
Gagaro <gagaro42@gmail.com>
2025-12-11 11:16:08 +0100

    day 10 part 2

--- day10/run.py
+++ day10/run.py
@@ -32,16 +32,54 @@ class DayRunner(Runner):
         return sum(counts)
 
     def run2(self):
+        total_counts = 0
         for line in self.lines:
-            print(line)
+            # print(line)
 
             line = line.split(' ')
-            buttons = list(map(lambda x: set(map(int, x[1:-1].split(','))), line[1:-1]))
-            joltages = line[-1][1:-1].split(',')
+            buttons = list(map(lambda x: map(int, x[1:-1].split(',')), line[1:-1]))
+            jolt_from_btn = {}
+            for button_id, jolts in enumerate(buttons):
+                for jolt in jolts:
+                    jolt_from_btn.setdefault(jolt, []).append(button_id)
 
+            joltages = list(map(int, line[-1][1:-1].split(',')))
 
+            o = Optimize()
 
-        return
+            count = Int('count')
+
+            B = [Int(f'b{i}') for i in range(len(buttons))]
+            for b in B:
+                o.add(b >= 0)
+
+            o.add(count == sum(B))
+
+            for joltage, joltage_buttons in jolt_from_btn.items():
+                o.add(joltages[joltage] == sum(B[button] for button in joltage_buttons))
+
+            # (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+            # {3: [0, 1, 3], 1: [1, 5], 2: [2, 3, 4], 0: [4, 5]}
+
+            # print(o)
+
+            # [b0 >= 0,
+            #  b1 >= 0,
+            #  b2 >= 0,
+            #  b3 >= 0,
+            #  b4 >= 0,
+            #  b5 >= 0,
+            #  count == b0 + b1 + b2 + b3 + b4 + b5,
+            #  0 + b0 + b1 + b3 == 7,
+            #  0 + b1 + b5 == 5,
+            #  0 + b2 + b3 + b4 == 4,
+            #  0 + b4 + b5 == 3]
+
+            o.minimize(count)
+            o.check()
+            total_counts += o.model()[count].as_long()
+
+        return total_counts
 
 
 if __name__ == '__main__':
