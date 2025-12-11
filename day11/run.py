1ab9589dcf60e61e3b7828600163e39735820015
Gagaro <gagaro42@gmail.com>
2025-12-11 19:56:58 +0100

    day 10 part 2

--- day11/run.py
+++ day11/run.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+
+import networkx as nx
 from pathlib import Path
 import sys
 
@@ -7,10 +9,37 @@ from runner import Runner
 
 class DayRunner(Runner):
     def run1(self):
-        return
+        graph = nx.DiGraph()
+
+        for line in self.lines:
+            node = line[:3]
+            for other_node in line[5:].split(' '):
+                graph.add_edge(node, other_node)
+
+        return len(list(nx.all_simple_paths(graph, 'you', 'out')))
 
     def run2(self):
-        return
+        graph = nx.DiGraph()
+
+        for line in self.lines:
+            node = line[:3]
+            for other_node in line[5:].split(' '):
+                graph.add_edge(node, other_node)
+
+        def count(start, end):
+            ordered_nodes = nx.topological_sort(graph)
+            counts = defaultdict(int)
+            counts[start] = 1
+            for node in ordered_nodes:
+                for child in graph.successors(node):
+                    counts[child] += counts[node]
+            return counts[end]
+
+        first_part = count('svr', 'fft')
+        seconde_part = count('fft', 'dac')
+        third_part = count('dac', 'out')
+
+        return first_part * seconde_part * third_part
 
 
 if __name__ == '__main__':
@@ -20,5 +49,5 @@ if __name__ == '__main__':
     else:
         input_path = Path(__file__).parent / 'input.txt'
 
-    print(DayRunner(input_path, test=is_test).run1())
+    # print(DayRunner(input_path, test=is_test).run1())
     print(DayRunner(input_path, test=is_test).run2())
