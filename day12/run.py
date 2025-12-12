77bdf7ed29c86e73627350c87cc86b138608b130
Gagaro <gagaro42@gmail.com>
2025-12-12 11:59:15 +0100

    day 12

--- day12/run.py
+++ day12/run.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from pathlib import Path
 import sys
 
@@ -7,7 +6,14 @@ from runner import Runner
 
 class DayRunner(Runner):
     def run1(self):
-        return
+        presents_areas = [5, 7, 7, 7, 6, 7]
+        count = 0
+        for line in self.lines[30:]:
+            area = int(line[:2]) * int(line[3:5])
+            needed_area = sum(a * b for a, b in zip(presents_areas, map(int, line[7:].split())))
+            if area >= needed_area:
+                count += 1
+        return count
 
     def run2(self):
         return
